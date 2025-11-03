7f419992cf6d3d4377a1d41177cf704713df809b
Ashwath S <miniash123@outlook.com>
2025-11-03 22:50:28 +0530

    Final commit

--- main.py
+++ main.py
@@ -8,10 +8,42 @@ screen.addshape(image)
 turtle.shape(image)
 
 states_data = pandas.read_csv("50_states.csv")
-print(states_data)
+score = 0
+guessed_states = []
+missing_states = []
+# print(states_data)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+states_list = states_data.state.to_list()
+# print(states_list)
 
+for i in range(0, len(states_list)):
+    answer_state = screen.textinput(title=f"{score} / {len(states_list)} States Correct",
+                                    prompt="What's another state's name?").title()
+    print(answer_state)
 
-screen.exitonclick()
+    if answer_state == "Exit":
+        for state in states_list:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        pandas.DataFrame(missing_states).to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in states_list:
+        guessed_states.append(answer_state)
+
+        x_cor = states_data[states_data.state == answer_state].x
+        y_cor = states_data[states_data.state == answer_state].y
+        coordinates = (int(x_cor), int(y_cor))
+        print(coordinates)
+
+        tim = turtle.Turtle()
+        tim.hideturtle()
+        tim.penup()
+        tim.goto(coordinates)
+        tim.write(answer_state, align="center", font=("Courier", 8, "normal"))
+
+        score += 1
+
+    else:
+        print("Wrong Answer...")
